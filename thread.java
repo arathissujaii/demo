50c0bd0edd54ec8a30589cc3588613897a3e19b6
arathissujaii <arathissujai04@gmail.com>
2021-04-22 11:34:55 +0530

    Second commit

---
+++ thread.java
@@ -0,0 +1,16 @@
+class threadtask{
+    public static void main(String args[]){
+        Thread t1=new Thread(){
+            public void run(){
+                System.out.println("Task One");
+            }
+        };
+        Thread t2=new Thread(){
+            public void run(){
+                System.out.println("Task Two");
+            }
+        };
+        t1.start();
+        t2.start();
+    }
+}
